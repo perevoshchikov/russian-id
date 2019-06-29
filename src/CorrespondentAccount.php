abaf93076d78929474a5dd9af096fe45a4e1d76b
Anton Perevoshchikov <anper3.5@gmail.com>
2019-06-29 15:36:59 +0300

    Adds correspondent account

---
+++ src/CorrespondentAccount.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Anper\IdValidation;
+
+/**
+ * Валидирует корреспондентский счёт.
+ *
+ * @see http://www.consultant.ru/cons/cgi/online.cgi?req=doc;base=LAW;n=16053;fld=134;dst=4294967295#013609377551161095
+ */
+class CorrespondentAccount extends AbstractAccount
+{
+    /**
+     * @inheritDoc
+     */
+    protected function getPrefix(): string
+    {
+        return '0' .$this->bik[4] .$this->bik[5];
+    }
+}
---
+++ tests/CorrespondentAccountTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Anper\IdValidation\Tests;
+
+use Anper\IdValidation\CorrespondentAccount;
+use PHPUnit\Framework\TestCase;
+
+class CorrespondentAccountTest extends TestCase
+{
+    public const BIK = '044030653';
+    public const ACCOUNT = '30101810500000000653';
+
+    public function testValid()
+    {
+        $ca = new CorrespondentAccount(static::BIK, static::ACCOUNT);
+
+        $this->assertTrue($ca->validate());
+    }
+
+    public function testInvalidBik()
+    {
+        $ca = new CorrespondentAccount(\implode('', \array_fill(0, 9, '0')), static::ACCOUNT);
+
+        $this->assertFalse($ca->validate());
+    }
+
+    public function testInvalidKs()
+    {
+        $ca = new CorrespondentAccount(static::BIK, \implode('', \array_fill(0, 20, '0')));
+
+        $this->assertFalse($ca->validate());
+    }
+
+    public function testInvalidBikLength()
+    {
+        $ca = new CorrespondentAccount('0', static::ACCOUNT);
+
+        $this->assertFalse($ca->validate());
+    }
+
+    public function testInvalidKsLength()
+    {
+        $ca = new CorrespondentAccount(static::BIK, '0');
+
+        $this->assertFalse($ca->validate());
+    }
+}
