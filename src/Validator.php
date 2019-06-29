797c2e42608afb94d30788f23295e21477c029ec
Anton Perevoshchikov <anper3.5@gmail.com>
2019-06-29 20:10:39 +0300

    Adds validator

---
+++ src/Validator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Anper\IdValidation;
+
+class Validator
+{
+    /**
+     * @param string $bik
+     * @param string $rs
+     *
+     * @return bool
+     */
+    public static function isRs(string $bik, string $rs): bool
+    {
+        return (new Rs($bik, $rs))->validate();
+    }
+
+    /**
+     * @param string $bik
+     * @param string $ks
+     *
+     * @return bool
+     */
+    public static function isKs(string $bik, string $ks): bool
+    {
+        return (new Ks($bik, $ks))->validate();
+    }
+
+    /**
+     * @param string $inn
+     *
+     * @return bool
+     */
+    public static function isInn(string $inn): bool
+    {
+        return (new Inn($inn))->validate();
+    }
+
+    /**
+     * @param string $kpp
+     *
+     * @return bool
+     */
+    public static function isKpp(string $kpp): bool
+    {
+        return (new Kpp($kpp))->validate();
+    }
+
+    /**
+     * @param string $ogrn
+     *
+     * @return bool
+     */
+    public static function isOgrn(string $ogrn): bool
+    {
+        return (new Ogrn($ogrn))->validate();
+    }
+
+    /**
+     * @param string $oms
+     *
+     * @return bool
+     */
+    public static function isOms(string $oms): bool
+    {
+        return (new Oms($oms))->validate();
+    }
+
+    /**
+     * @param string $snils
+     *
+     * @return bool
+     */
+    public static function isSnils(string $snils): bool
+    {
+        return (new Snils($snils))->validate();
+    }
+}
---
+++ tests/ValidatorTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Anper\IdValidation\Tests;
+
+use Anper\IdValidation\Validator;
+use PHPUnit\Framework\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testValidInn()
+    {
+        $this->assertTrue(Validator::isInn('500100732259'));
+    }
+
+    public function testInvalidInn()
+    {
+        $this->assertFalse(Validator::isInn('500100732250'));
+    }
+
+    public function testValidKpp()
+    {
+        $this->assertTrue(Validator::isKpp('514944513'));
+    }
+
+    public function testInvalidKpp()
+    {
+        $this->assertFalse(Validator::isKpp('51494451A'));
+    }
+
+    public function testValidKs()
+    {
+        $this->assertTrue(Validator::isKs('044030653', '30101810500000000653'));
+    }
+
+    public function testInvalidKs()
+    {
+        $this->assertFalse(Validator::isKs('044030653', '30101810500000000650'));
+    }
+
+    public function testValidRs()
+    {
+        $this->assertTrue(Validator::isRs('044030653', '40702810955230165464'));
+    }
+
+    public function testInvalidRs()
+    {
+        $this->assertFalse(Validator::isRs('044030653', '40702810955230165460'));
+    }
+
+    public function testValidOgrn()
+    {
+        $this->assertTrue(Validator::isOgrn('1138218667113'));
+    }
+
+    public function testInvalidOgrn()
+    {
+        $this->assertFalse(Validator::isOgrn('1138218667110'));
+    }
+
+    public function testValidOms()
+    {
+        $this->assertTrue(Validator::isOms('2341998071655749'));
+    }
+
+    public function testInvalidOms()
+    {
+        $this->assertFalse(Validator::isOms('2341998071655740'));
+    }
+
+    public function testValidSnils()
+    {
+        $this->assertTrue(Validator::isSnils('11223344595'));
+    }
+
+    public function testInvalidSnils()
+    {
+        $this->assertFalse(Validator::isSnils('11223344590'));
+    }
+}
