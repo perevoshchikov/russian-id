7e2f8887236ea2847f4c083d571f830f197ee609
Anton Perevoshchikov <anper3.5@gmail.com>
2021-10-01 14:53:13 +0300

    Update validation test

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -7,113 +7,61 @@ use PHPUnit\Framework\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    public function testValidInnAsPerson(): void
+    public function validProvider(): array
     {
-        $this->assertTrue(Validator::isInn('500100732259'));
+        return [
+            ['isBik', ['044030653']],
+            ['isRs', ['044030653', '40702810955230165464']],
+            ['isKs', ['044030653', '30101810500000000653']],
+            ['isInn', ['7830002293']],
+            ['isInn', ['500100732259']],
+            ['isLegalInn', ['7830002293']],
+            ['isPersonInn', ['500100732259']],
+            ['isKpp', ['514944513']],
+            ['isOgrn', ['1151232294620']],
+            ['isOgrnip', ['315850060115169']],
+            ['isOgrnOrOgrnip', ['315850060115169']],
+            ['isOgrnOrOgrnip', ['1151232294620']],
+            ['isOms', ['2341998071655749']],
+            ['isSnils', ['11223344595']],
+        ];
     }
 
-    public function testValidInnAsLegal(): void
+    public function invalidProvider(): array
     {
-        $this->assertTrue(Validator::isInn('7830002293'));
+        return [
+            ['isBik', ['0']],
+            ['isRs', ['044030653', '0']],
+            ['isKs', ['044030653', '0']],
+            ['isRs', ['0', '40702810955230165464']],
+            ['isKs', ['0', '30101810500000000653']],
+            ['isInn', ['0']],
+            ['isInn', ['0']],
+            ['isLegalInn', ['0']],
+            ['isPersonInn', ['0']],
+            ['isKpp', ['0']],
+            ['isOgrn', ['0']],
+            ['isOgrnip', ['0']],
+            ['isOgrnOrOgrnip', ['0']],
+            ['isOgrnOrOgrnip', ['0']],
+            ['isOms', ['0']],
+            ['isSnils', ['0']],
+        ];
     }
 
-    public function testValidPerosnInn(): void
+    /**
+     * @dataProvider validProvider
+     */
+    public function testValid(string $method, array $args): void
     {
-        $this->assertTrue(Validator::isPersonInn('500100732259'));
+        $this->assertTrue(\call_user_func_array([Validator::class, $method], $args));
     }
 
-    public function testValidLegalInn(): void
+    /**
+     * @dataProvider invalidProvider
+     */
+    public function testInvalid(string $method, array $args): void
     {
-        $this->assertTrue(Validator::isLegalInn('7830002293'));
-    }
-
-    public function testInvalidInn(): void
-    {
-        $this->assertFalse(Validator::isInn('7830002291'));
-    }
-
-    public function testInvalidPersonInn(): void
-    {
-        $this->assertFalse(Validator::isPersonInn('500100732250'));
-    }
-
-    public function testInvalidLegalInn(): void
-    {
-        $this->assertFalse(Validator::isLegalInn('500100732250'));
-    }
-
-    public function testValidKpp(): void
-    {
-        $this->assertTrue(Validator::isKpp('514944513'));
-    }
-
-    public function testInvalidKpp(): void
-    {
-        $this->assertFalse(Validator::isKpp('51494451A'));
-    }
-
-    public function testValidKs(): void
-    {
-        $this->assertTrue(Validator::isKs('044030653', '30101810500000000653'));
-    }
-
-    public function testInvalidKs(): void
-    {
-        $this->assertFalse(Validator::isKs('044030653', '30101810500000000650'));
-    }
-
-    public function testValidRs(): void
-    {
-        $this->assertTrue(Validator::isRs('044030653', '40702810955230165464'));
-    }
-
-    public function testInvalidRs(): void
-    {
-        $this->assertFalse(Validator::isRs('044030653', '40702810955230165460'));
-    }
-
-    public function testValidOgrn(): void
-    {
-        $this->assertTrue(Validator::isOgrn('1138218667113'));
-    }
-
-    public function testInvalidOgrn(): void
-    {
-        $this->assertFalse(Validator::isOgrn('1138218667110'));
-    }
-
-    public function testInvalidOgrnip(): void
-    {
-        $this->assertFalse(Validator::isOgrn('315850060115169'));
-    }
-
-    public function testValidOms(): void
-    {
-        $this->assertTrue(Validator::isOms('2341998071655749'));
-    }
-
-    public function testInvalidOms(): void
-    {
-        $this->assertFalse(Validator::isOms('2341998071655740'));
-    }
-
-    public function testValidSnils(): void
-    {
-        $this->assertTrue(Validator::isSnils('11223344595'));
-    }
-
-    public function testInvalidSnils(): void
-    {
-        $this->assertFalse(Validator::isSnils('11223344590'));
-    }
-
-    public function testValidBik(): void
-    {
-        $this->assertTrue(Validator::isBik('044525225'));
-    }
-
-    public function testInvalidBik(): void
-    {
-        $this->assertFalse(Validator::isBik('04452522a'));
+        $this->assertFalse(\call_user_func_array([Validator::class, $method], $args));
     }
 }
